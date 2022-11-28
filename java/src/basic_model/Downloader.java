322186c2d07f1a0333a5d89f35ff1620f17174c3
cogito1016 <cogito1016@naver.com>
2022-11-28 20:00:50 +0900

    feat: Downloader

---
+++ java/src/basic_model/Downloader.java
@@ -0,0 +1,52 @@
+package basic_model;
+
+import sun.net.ProgressListener;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Downloader extends Thread{
+    private InputStream in;
+    private OutputStream out;
+    private ArrayList<ProgressListener> listeners;
+
+    public Downloader(URL url, String outputFilename) throws IOException{
+        in = url.openConnection().getInputStream();
+        out = new FileOutputStream(outputFilename);
+        listeners = new ArrayList<ProgressListener>();
+    }
+
+    public synchronized void addListener(ProgressListener listener){
+        listeners.add(listener);
+    }
+
+    public synchronized void removeListener(ProgressListener listener){
+        listeners.remove(listener);
+    }
+
+    public synchronized void updateProgress(int n){
+        for(ProgressListener progressListener : listeners){
+            progressListener.onProgress(n);
+        }
+    }
+
+    public void run(){
+        int n = 0, total = 0;
+        byte[] buffer = new byte[1024];
+
+        try{
+            while((n = in.read(buffer)) != -1){
+                out.write(buffer, 0 , n);
+                total += n;
+                updateProgress(total);
+            }
+            out.flush();
+        }catch (IOException e){
+
+        }
+    }
+}
