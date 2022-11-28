57ee71e997c44c40b1a9a1494047ad57b08488ea
cogito1016 <cogito1016@naver.com>
2022-11-28 18:53:30 +0900

    feat: Puzzle

---
+++ java/src/basic_model/Puzzle.java
@@ -0,0 +1,30 @@
+package basic_model;
+
+public class Puzzle {
+    static boolean answerReady = false;
+    static int answer = 0;
+    static Thread t1 = new Thread(){
+      public void run(){
+          answer = 42;
+          answerReady = true;
+      }
+    };
+
+    static Thread t2 = new Thread(){
+      public void run(){
+          if(answerReady){
+              System.out.println("The answer is : " + answer);
+          }else{
+              System.out.println("I don't know answer");
+          }
+      }
+    };
+
+    public static void main(String[] args) throws InterruptedException {
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+    }
+}
